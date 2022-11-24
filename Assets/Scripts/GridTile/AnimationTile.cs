993f921e7158f03e079c7900336909741c74171e
Vittorio Chiuselli <vittorio.chiuselli@gmail.com>
2022-11-24 19:52:15 +0100

    Draft to add animation

---
+++ Assets/Scripts/GridTile/AnimationTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.GridTile
+{
+    public class AnimationTile : MonoBehaviour
+    {
+
+        private void Update()
+        {
+            if (GetComponent<CoordinateSystem>().typeOfTile == 7)
+            {
+                //TODO trigger animation
+            }
+            else
+            {
+                //TODO stop animation
+            }
+        }
+
+    }
+}
